09ed1c11adb1c0f8106b77a023361f37ab79a12d
jpsimos <jpsimos@gmail.com>
2025-05-14 22:50:41 -0400

    Added basic unit test to verify the algorithms.

---
+++ Test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "Crc32.h"
+#include "Crypto.h"
+
+int main(int argc, char* argv[])
+{
+	const char* testData = "A rhino on the Sahara.";
+	const char* testDataKey = "Is a force";
+	const size_t testDataLength = strlen(testData);
+	const size_t testDataKeyLength = strlen(testDataKey);
+	const uint32_t testDataChecksum = CalculateCrc32(testData, testDataLength, 0xEDB88320U);
+	char encryptedTestData[100];
+	char decryptedTestData[100];
+	R4CKey encryptoKey;
+	R4CKey otherEncryptoKey;
+	
+	if(0xCEED4F9DU != testDataChecksum)
+	{
+		puts("Fail checksum");
+		return 1;
+	}
+	
+	R4CDeriveKey(&encryptoKey, testData, testDataKeyLength);
+	R4CDeriveKey(&otherEncryptoKey, testData, testDataKeyLength);
+	
+	R4CCrypt(&encryptoKey, testData, testDataLength, encryptedTestData);
+	R4CCrypt(&otherEncryptoKey, encryptedTestData, testDataLength, decryptedTestData);
+	
+	if(memcmp(testData, decryptedTestData, testDataLength))
+	{
+		puts("Fail crypto");
+		return 2;
+	}
+	
+	puts("Pass");
+
+	return 0;
+}
